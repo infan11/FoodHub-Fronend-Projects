85e5462e0ddf8cef94ed3a87a7587c39f31d7eee
infan11 <infanjiounrahman20606@gmail.com>
2024-12-04 12:02:10 +0600

    added all routes

---
+++ src/Components/Restaurants/Restaurants/Restaurants.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Restaurants = () => {
+    return (
+        <div>
+           this is Restaurants page 
+        </div>
+    );
+};
+
+export default Restaurants;
--- src/Components/Routes/Routes/Routes.jsx
+++ src/Components/Routes/Routes/Routes.jsx
@@ -1,12 +1,45 @@
 import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
-  
- export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
- 
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from "../../Main/Main";
+import Home from "../../Home/Home/Home";
+import Restaurants from "../../Restaurants/Restaurants/Restaurants";
+import Food from "../../Food/Food/Food";
+import About from "../../About/About/About";
+import Login from "../../Auth/Login/Login";
+import Register from "../../Auth/Register/Register/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    // TO DO : ERROR element
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/restaurants",
+        element: <Restaurants />
+      },
+      {
+        path: "/food",
+        element: <Food />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/register",
+        element: <Register />
+      }
+    ]
+  },
+]);
