f7318f4e8e329bf83d5c0a468539f285d49b9f35
infan11 <infanjiounrahman20606@gmail.com>
2024-12-07 21:15:56 +0600

    add login page

--- src/Components/Auth/Login/Login.jsx
+++ src/Components/Auth/Login/Login.jsx
@@ -1,9 +1,98 @@
-import React from 'react';
-
+import {
+    Card,
+    Input,
+    Checkbox,
+    Button,
+    Typography,
+} from "@material-tailwind/react";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
 const Login = () => {
+    const { register, handleSubmit, watch, formState: { errors }, } = useForm()
+    const onSubmit = event => {
+        console.log(event);
+    }
     return (
-        <div>
-            This is login page 
+        <div className="">
+            <div className="hero  min-h-screen  max-w-7xl mx-auto md:px-5">
+                <div data-aos="zoom-in" className=" grid md:grid-cols-2 rounded-r-2xl shadow-2xl">
+
+                    <div className="  lg:w-full bg-white shrink-0 rounded-r-2xl shadow-2xl">
+                        <div className="card-body">
+                            <Card color="  transparent" className="" shadow={false}>
+                                <p className="text-2xl font-extrabold mt-8 text-center mb-3 transition-all "> SIGNUP </p>
+
+
+
+                                <form onSubmit={handleSubmit(onSubmit)} className="mt-3 mb-2 w-80 mx-auto max-w-screen-lg sm:w-80">
+                                    <div className="mb-1 flex flex-col gap-6">
+
+
+
+                                        <Input
+                                            size="lg"
+                                            name="email"
+                                            type="email"
+                                            label="Your Email"
+                                            {...register("email", { required: true })}
+                                        />
+                                        {errors.email && <span className="text-red-600 text-sm font-bold">This field is required</span>}
+
+                                        <Input
+                                            type="password"
+                                            name="password"
+                                            size="lg"
+                                            placeholder="********"
+                                            className=""
+                                            label="Password"
+                                            {...register("password", { required: true, minLength: 6, maxLength: 8 })}
+                                        />
+                                        {errors.password?.type && <span className="text-red-600">This field is reqiure</span>}
+                                        {errors.password?.type === 'minLength' && <span className="text-red-600">This pass must 6 Characters</span>}
+                                        {errors.password?.type === 'maxLength' && <span className="text-red-600">This pass only 8 Characters</span>}
+                                    </div>
+                                    <Checkbox
+                                        label={
+                                            <Typography
+                                                variant="small"
+                                                color="gray"
+                                                className="flex items-center font-normal"
+                                            >
+                                                I agree the
+                                                <a
+                                                    href="#"
+                                                    className="font-medium transition-colors hover:text-gray-900"
+                                                >
+                                                    &nbsp;Terms and Conditions
+                                                </a>
+                                            </Typography>
+
+                                        }
+                                        containerProps={{ className: "-ml-2.5" }}
+                                    />
+                                    <br />
+                                    <button className=" w-full uppercase bg-[#ea9540fd] hover:bg-[#ea9540fd] text-white mt-2 btn rounded-badge" fullWidth>
+                                        sign up
+                                    </button>
+                                    <div className="divider">OR</div>
+                                </form>
+
+                                <div className=" mx-auto "> <button className="flex text-[14px] items-center font-bold btn rounded-full"><FcGoogle /> Continue With Google</button></div>
+                                <Typography color="gray" className="mt-4 text-center font-normal">
+                                    Create a new account?{" "}
+                                    <a href="/register" className="font-medium text-gray-900">
+                                        Sign in
+                                    </a>
+                                </Typography>
+
+                            </Card>
+                        </div>
+                    </div>
+                    <div className="text-center hidden sm:block ">
+                        <img className=" md:w-[600px] md:h-[600px] lg:w-[680px] lg:h-[680px]   rounded-l-2xl" src="https://i.ibb.co.com/F6hmykd/Login.png" alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Auth/Register/Register/Register.jsx
+++ src/Components/Auth/Register/Register/Register.jsx
@@ -4,83 +4,101 @@ import {
     Checkbox,
     Button,
     Typography,
-  } from "@material-tailwind/react";
-  import { FcGoogle } from "react-icons/fc";
+} from "@material-tailwind/react";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
 const Register = () => {
+    const { register, handleSubmit, watch, formState: { errors }, } = useForm()
+    const onSubmit = event => {
+        console.log(event);
+    }
     return (
-        <div className="bg-[#ea9540fd]">
-          <div className="hero  min-h-screen  max-w-7xl mx-auto">
-  <div data-aos="zoom-in" className=" grid md:grid-cols-2 rounded-r-2xl shadow-2xl">
-    <div className="text-center ">
-     <img className="w-[500px] h-[500px] rounded-l-2xl" src="https://i.ibb.co.com/TqjSXw8/Register.png" alt="" />
-    </div>
-    <div className="  w-full bg-white shrink-0 rounded-r-2xl shadow-2xl">
-      <form className="card-body">
-      <Card color="transparent" shadow={false}>
-     
-     
-      <form className="mt-3 mb-2 w-80 mx-auto max-w-screen-lg sm:w-96">
-        <div className="mb-1 flex flex-col gap-6">
-    
-          <Input
-            size="lg"
-            name="name"
-            className=" "
-            label="Your Name"
-            
-          />
-        
-          <Input
-            size="lg"
-            name="email"
-            className=" "
-          label="Your Email"
-          />
-      
-          <Input
-            type="password"
-            size="lg"
-            placeholder="********"
-            className=" "
-          label="Password"
-          />
-        </div>
-        <Checkbox
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
-              I agree the
-              <a
-                href="#"
-                className="font-medium transition-colors hover:text-gray-900"
-              >
-                &nbsp;Terms and Conditions
-              </a>
-            </Typography>
+        <div className="">
+            <div className="hero  min-h-screen  max-w-7xl mx-auto md:px-5">
+                <div data-aos="zoom-in" className=" grid md:grid-cols-2 rounded-r-2xl shadow-2xl">
+                    <div className="text-center hidden sm:block ">
+                        <img className=" md:w-[650px] md:h-[600px] lg:w-[685px] lg:h-[685px]   rounded-l-2xl" src="https://i.ibb.co.com/TqjSXw8/Register.png" alt="" />
+                    </div>
+                    <div className="  lg:w-full bg-white shrink-0 rounded-r-2xl shadow-2xl">
+                        <div className="card-body">
+                            <Card color="  transparent" className="" shadow={false}>
+                                <p className="text-2xl font-extrabold text-center mb-3 transition-all "> SIGNIN </p>
 
-          }
-          containerProps={{ className: "-ml-2.5" }}
-        />
-        <Button className="mt-2" fullWidth>
-          sign up
-        </Button>
-        <div className="divider">OR</div>
-           <div className="w-72 mx-auto ml-20"> <button className="flex text-[14px] items-center font-bold btn rounded-full"><FcGoogle /> Continue With Google</button></div>
-        <Typography color="gray" className="mt-4 text-center font-normal">
-          Already have an account?{" "}
-          <a href="/login" className="font-medium text-gray-900">
-            Sign In
-          </a>
-        </Typography>
-      </form>
-    </Card>
-      </form>
-    </div>
-  </div>
-</div>
+
+
+                                <form onSubmit={handleSubmit(onSubmit)} className="mt-3 mb-2 w-80 mx-auto max-w-screen-lg sm:w-80">
+                                    <div className="mb-1 flex flex-col gap-6">
+
+                                        <Input size="lg"
+                                            name="name"
+                                            type="name"
+                                            label="Your Name"
+                                            {...register("name", { required: true })}
+
+                                        />
+                                        {errors.name && <span className="text-red-600 text-sm font-bold">This field is required</span>}
+
+                                        <Input
+                                            size="lg"
+                                            name="email"
+                                            type="email"
+                                            label="Your Email"
+                                            {...register("email", { required: true })}
+                                        />
+                                        {errors.email && <span className="text-red-600 text-sm font-bold">This field is required</span>}
+
+                                        <Input
+                                            type="password"
+                                            name="password"
+                                            size="lg"
+                                            placeholder="********"
+                                            className=""
+                                            label="Password"
+                                            {...register("password",  {required : true,  minLength: 6, maxLength: 8  })}
+                                        />
+                                        {errors.password?.type && <span className="text-red-600">This field is reqiure</span>}
+                                        {errors.password?.type === 'minLength' && <span className="text-red-600">This pass must 6 Characters</span>}
+                                        {errors.password?.type === 'maxLength' && <span className="text-red-600">This pass only 8 Characters</span>}
+                                    </div>
+                                    <Checkbox
+                                        label={
+                                            <Typography
+                                                variant="small"
+                                                color="gray"
+                                                className="flex items-center font-normal"
+                                            >
+                                                I agree the
+                                                <a
+                                                    href="#"
+                                                    className="font-medium transition-colors hover:text-gray-900"
+                                                >
+                                                    &nbsp;Terms and Conditions
+                                                </a>
+                                            </Typography>
+
+                                        }
+                                        containerProps={{ className: "-ml-2.5" }}
+                                    />
+                                    <br />
+                                    <button className=" w-full uppercase bg-[#ea9540fd] hover:bg-[#ea9540fd] text-white mt-2 btn rounded-badge" fullWidth>
+                                        sign in
+                                    </button>
+                                    <div className="divider">OR</div>
+                                </form>
+
+                                <div className=" mx-auto "> <button className="flex text-[14px] items-center font-bold btn rounded-full"><FcGoogle /> Continue With Google</button></div>
+                                <Typography color="gray" className="mt-4 text-center font-normal">
+                                    Already have an account?{" "}
+                                    <a href="/login" className="font-medium text-gray-900">
+                                        Sign up
+                                    </a>
+                                </Typography>
+
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
